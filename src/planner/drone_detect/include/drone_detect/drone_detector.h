ad98b3ab1e2edbbef7d109fba5d11151d8fe4f0e
zhaoli <zhaoli.go@gmail.com>
2025-12-08 18:19:50 +0400

    refactor: Conditionally include cv_bridge header for improved compatibility.

--- src/planner/drone_detect/include/drone_detect/drone_detector.h
+++ src/planner/drone_detect/include/drone_detect/drone_detector.h
@@ -23,7 +23,11 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/eigen.hpp>
+#if __has_include(<cv_bridge/cv_bridge.hpp>)
 #include <cv_bridge/cv_bridge.hpp>
+#else
+#include <cv_bridge/cv_bridge.h>
+#endif
 
 namespace detect
 {
